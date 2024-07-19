d75b7987e1589693d1f979a7c188100294660e55
TRI-M\jmaric <josip.maric@tri-m.hr>
2024-07-19 15:16:56 +0200

    temp

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,21 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import vueJsx from '@vitejs/plugin-vue-jsx';
-import vueDevTools from 'vite-plugin-vue-devtools';
-import vueFacingDecoratorHmr from "./vite-plugin-vue-facing-decorator-hmr";
-//for use in build and test mode
+
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-    vueFacingDecoratorHmr(),
-  ],
-  server: {
-    hmr: {
-      overlay: true,
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: './src/index.ts',
+      name: 'VueComponentsPackage',
+      fileName: (format) => `vue-components-package.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
     },
   },
 });
